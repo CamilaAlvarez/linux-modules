342a45bf7724a507b69604d8cef9ba57fb0d6345
Camila Alvarez <cam.alvarez.i@gmail.com>
2024-02-11 21:40:12 -0300

    led module

---
+++ led_module/kernel/led_lkm.c
@@ -0,0 +1,109 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/miscdevice.h>
+#include <linux/device.h>
+#include <linux/fs.h>
+#include <linux/atomic.h>
+#include <linux/ioctl.h>
+#include "../led_lkm.h"
+
+#define MODNAME "led_test_lkm"
+#define READ_LENGTH 1
+static const struct file_operations led_lkm_fops = {
+    .llseek = no_llseek,
+    .open = open_led_lkm,
+    .read = read_led_lkm,
+    .release = close_led_lkm,
+    .ioctl = ioctl_led_lkm
+
+};
+static struct miscdevice led_lkm_miscdev = {
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = MODNAME,
+    .mode = 0666,
+    .fops = &led_lkm_fops,
+};
+struct ledLkmCtx {
+    struct device *dev;
+    atomic_t powered;
+};
+static struct ledLkmCtx *gpriv; 
+MODULE_AUTHOR("led experiment module");
+MODULE_DESCRIPTION("Testing how to use GPIO to activate and deactivate a led");
+MODULE_LICENSE("GPL");
+MODULE_VERSION("0.1");
+
+static int __init led_lkm_init(void)
+{
+    printk(KERN_INFO "Entering LED LKM module");
+    int ret;
+    struct device* dev;
+
+    ret = misc_register(&led_lkm_miscdev);
+    if (ret != 0) {
+        // Notice level = 5
+        pr_notice("misc device registration failed, aborting\n");
+        return ret;
+    }
+    dev = led_lkm_miscdev.this_device;
+    // kzalloc sets memory to 0
+    gpriv = dev_kzalloc(dev, sizeof(struct ledLkmCtx), GFP_KERNEL);
+    gpriv->dev = dev;
+    gpriv->powered = ATOMIC_INIT(0);
+    return 0;
+}
+
+static void __exit led_lkm_exit(void)
+    {
+    printk(KERN_INFO "Exiting LED LKM module");
+}
+
+static int open_led_lkm(struct inode *inode, struct file *flip) {
+    return nonseekable_open(inode, flip);
+}
+static ssize_t read_led_lkm(struct file *filp, char __user *ubuf, szie_t count, loff_t *oof) {
+    int ret, powered;
+    ret = -EFAULT;
+    powered = atomic_read(gpriv->powered);
+    if (copy_to_user(ubuf, powered, READ_LENGTH)) {
+        goto out;
+    }
+    ret = READ_LENGTH;
+out:
+    return ret;
+}
+static int close_led_lkm(struct inode *inode, struct file *flip) {
+    return 0;
+}
+static long ioctl_led_lkm(struct file *filp, unsigned int cmd, unsigned long arg) {
+    int retval = 0;
+    pr_debug("In ioctl method, cmd=%d\n", _IOC_NR(cmd));
+    // verify the message if for us
+    if (_IOC_TYPE(cmd) != IOCTL_LED_LKM_MAGIC) {
+        pr_info("ioctl fail; magin number mismatch\n");
+        return -ENOTTY;
+    }
+    if (_IOC_NR(cmd) > IOCTL_MAXCMD_LED) {
+        pr_info("ioctl failed; invalid cmd?\n");
+        return -ENNOTY;
+    }
+    switch (cmd) {
+        case IOCTL_POWER_ON:
+            pr_debug("Power on\n");
+            break;
+        case IOCTL_POWER_OFF:
+            pr_debug("Power off\n");
+            break;
+        case IOCTL_POWER_READ:
+            pr_debug("Read power value\n");
+            break;
+        default:
+            return -ENOTTY;
+    return retval;
+    }
+
+}
+module_init(led_lkm_init);
+module_exit(led_lkm_exit);
+
---
+++ led_module/user/led_lkm_user.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <unistd.h> // exit
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h> //O_RDWR	
+#include <signal.h>
+#include <stdlib.h> // malloc, free, y otras
+#include <sys/ioctl.h>
+#include "../led_lkm.h"
+
+int main(int argc, char **argv) {
+    int fd, power;
+
+    if (argc < 2) {
+        frpintf(stderr, "Usage: %s device_file\n\
+  If device_file does not exist, create it using mknod(1) (as root)\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    if((fd = open(argv[1], O_RDWR, 0)) == -1) {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
+
+    if (ioctl(fd, IOCTL_POWER_ON, 0) == -1) {
+        perror("ioctl IOCTL_POWER_ON failed"); 
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+
+    if (ioctl(fd, IOCTL_POWER_OFF, 0) == -1) {
+        perror("ioctl IOCTL_POWER_OFF failed"); 
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+
+    if (ioctl(fd, IOCTL_POWER_READ, &power) == -1) {
+        perror("ioctl IOCTL_POWER_READ failed"); 
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+    close(fd);
+	exit(EXIT_SUCCESS);
+
+}
