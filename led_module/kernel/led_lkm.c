4ca69dca3a89e20ba522856b9340a909c044f3cc
Camila Alvarez <cam.alvarez.i@gmail.com>
2024-02-13 15:44:36 -0300

    Change base address for GPIO and remove request mem. User program works in a loop

--- led_module/kernel/led_lkm.c
+++ led_module/kernel/led_lkm.c
@@ -13,8 +13,11 @@
 
 #define MODNAME "led_test_lkm"
 #define READ_LENGTH sizeof(int)
-#define BASE_MEMORY_ADDRESS 0x7e200000
-#define RESOURCE_SIZE 232 //58*4 
+// NOTE: The 0x7e200000 is the base address in the bus address space
+#define PERIPH_BASE 0xFE000000
+#define GPIO_BASE (PERIPH_BASE + 0x200000)
+#define BASE_MEMORY_ADDRESS GPIO_BASE
+#define RESOURCE_SIZE 232 // 58*4
 #define GPIO_PIN_OFFSET 0x08
 #define GPIO_SET_PIN_OFFSET 0x1c
 #define GPIO_CLEAR_PIN_OFFSET 0x28
@@ -24,72 +27,81 @@ MODULE_AUTHOR("led experiment module");
 MODULE_DESCRIPTION("Testing how to use GPIO to activate and deactivate a led");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0.1");
-struct ledLkmCtx {
+struct ledLkmCtx
+{
     struct device *dev;
     atomic_t powered;
     void *base_io;
     struct mutex mutex_mmio;
 };
-static struct ledLkmCtx *gpriv; 
+static struct ledLkmCtx *gpriv;
 
-static int open_led_lkm(struct inode *inode, struct file *flip) {
+static int open_led_lkm(struct inode *inode, struct file *flip)
+{
     return nonseekable_open(inode, flip);
 }
-static ssize_t read_led_lkm(struct file *filp, char __user *ubuf, size_t count, loff_t *oof) {
+static ssize_t read_led_lkm(struct file *filp, char __user *ubuf, size_t count, loff_t *oof)
+{
     int ret, powered;
     ret = -EFAULT;
     powered = atomic_read(&gpriv->powered);
-    if (copy_to_user(ubuf, &powered, READ_LENGTH)) {
+    if (copy_to_user(ubuf, &powered, READ_LENGTH))
+    {
         goto out;
     }
     ret = READ_LENGTH;
 out:
     return ret;
 }
-static int close_led_lkm(struct inode *inode, struct file *flip) {
+static int close_led_lkm(struct inode *inode, struct file *flip)
+{
     return 0;
 }
-static long ioctl_led_lkm(struct file *filp, unsigned int cmd, unsigned long arg) {
+static long ioctl_led_lkm(struct file *filp, unsigned int cmd, unsigned long arg)
+{
     int retval = 0;
     pr_debug("In ioctl method, cmd=%d\n", _IOC_NR(cmd));
     // verify the message if for us
-    if (_IOC_TYPE(cmd) != IOCTL_LED_LKM_MAGIC) {
+    if (_IOC_TYPE(cmd) != IOCTL_LED_LKM_MAGIC)
+    {
         pr_info("ioctl fail; magin number mismatch\n");
         return -ENOTTY;
     }
-    if (_IOC_NR(cmd) > IOCTL_MAXCMD_LED) {
+    if (_IOC_NR(cmd) > IOCTL_MAXCMD_LED)
+    {
         pr_info("ioctl failed; invalid cmd?\n");
         return -ENOTTY;
     }
     u32 mask = 1 << 22;
     u32 gpset0;
-    switch (cmd) {
-        case IOCTL_POWER_ON:
-            pr_debug("Power on\n");
-            mutex_lock(&gpriv->mutex_mmio);
-            gpset0 = ioread32(gpriv->base_io + GPIO_SET_PIN_OFFSET);
-            gpset0 |= mask;
-            iowrite32(gpset0, gpriv->base_io + GPIO_SET_PIN_OFFSET);
-            mutex_unlock(&gpriv->mutex_mmio);
-            break;
-        case IOCTL_POWER_OFF:
-            mutex_lock(&gpriv->mutex_mmio);
-            gpset0 = ioread32(gpriv->base_io + GPIO_CLEAR_PIN_OFFSET);
-            gpset0 |= mask;
-            iowrite32(gpset0, gpriv->base_io + GPIO_CLEAR_PIN_OFFSET);
-            mutex_unlock(&gpriv->mutex_mmio);
-            pr_debug("Power off\n");
-            break;
-        case IOCTL_POWER_READ:
-            mutex_lock(&gpriv->mutex_mmio);
-            gpset0 = ioread32(gpriv->base_io + GPIO_READ_PIN_OFFSET);
-            int value = gpset0 & mask;
-            mutex_unlock(&gpriv->mutex_mmio);
-            retval = __put_user(value > 0 ? 1 : 0, (int __user *)arg);
-            pr_debug("Read power value\n");
-            break;
-        default:
-            return -ENOTTY;
+    switch (cmd)
+    {
+    case IOCTL_POWER_ON:
+        pr_debug("Power on\n");
+        mutex_lock(&gpriv->mutex_mmio);
+        gpset0 = ioread32(gpriv->base_io + GPIO_SET_PIN_OFFSET);
+        gpset0 |= mask;
+        iowrite32(gpset0, gpriv->base_io + GPIO_SET_PIN_OFFSET);
+        mutex_unlock(&gpriv->mutex_mmio);
+        break;
+    case IOCTL_POWER_OFF:
+        mutex_lock(&gpriv->mutex_mmio);
+        gpset0 = ioread32(gpriv->base_io + GPIO_CLEAR_PIN_OFFSET);
+        gpset0 |= mask;
+        iowrite32(gpset0, gpriv->base_io + GPIO_CLEAR_PIN_OFFSET);
+        mutex_unlock(&gpriv->mutex_mmio);
+        pr_debug("Power off\n");
+        break;
+    case IOCTL_POWER_READ:
+        mutex_lock(&gpriv->mutex_mmio);
+        gpset0 = ioread32(gpriv->base_io + GPIO_READ_PIN_OFFSET);
+        int value = gpset0 & mask;
+        mutex_unlock(&gpriv->mutex_mmio);
+        retval = __put_user(value > 0 ? 1 : 0, (int __user *)arg);
+        pr_debug("Read power value\n");
+        break;
+    default:
+        return -ENOTTY;
     }
 
     return retval;
@@ -114,9 +126,10 @@ static int __init led_lkm_init(void)
     // Here I should mark the GPIO22 as output
     printk(KERN_INFO "Entering LED LKM module");
     int ret;
-    struct device* dev;
+    struct device *dev;
     ret = misc_register(&led_lkm_miscdev);
-    if (ret != 0) {
+    if (ret != 0)
+    {
         // Notice level = 5
         pr_notice("misc device registration failed, aborting\n");
         return ret;
@@ -125,14 +138,17 @@ static int __init led_lkm_init(void)
     // kzalloc sets memory to 0
     gpriv = devm_kzalloc(dev, sizeof(struct ledLkmCtx), GFP_KERNEL);
     gpriv->dev = dev;
-    // request memory
-    if (!request_mem_region(BASE_MEMORY_ADDRESS, RESOURCE_SIZE, MODNAME)){
+    // request memory: Note that this will always return NULL as someone is already using this memory.
+    // We shouldn't map GPIOs! we're only doing it as a test (we should use /sys/class/gio)
+    /*if (!request_mem_region(BASE_MEMORY_ADDRESS, RESOURCE_SIZE, MODNAME))
+    {
         dev_warn(dev, "couldn't get region for mmio, aborting\n");
         return -EBUSY;
-    }
+    }*/
     // map the memory
     gpriv->base_io = devm_ioremap(dev, BASE_MEMORY_ADDRESS, RESOURCE_SIZE);
-    if (gpriv->base_io == NULL){
+    if (gpriv->base_io == NULL)
+    {
         dev_err(dev, "register mapping failed\n");
         return -ENXIO;
     }
@@ -147,7 +163,8 @@ static int __init led_lkm_init(void)
     return 0;
 }
 
-static void __exit led_lkm_exit(void){
+static void __exit led_lkm_exit(void)
+{
     // reset pin to input
     u32 gpfsel2 = ioread32(gpriv->base_io + GPIO_PIN_OFFSET);
     u32 mask1 = ~(1 << 6 | 1 << 7 | 1 << 8);
@@ -159,4 +176,3 @@ static void __exit led_lkm_exit(void){
 
 module_init(led_lkm_init);
 module_exit(led_lkm_exit);
-
--- led_module/user/led_lkm_user.c
+++ led_module/user/led_lkm_user.c
@@ -2,53 +2,71 @@
 #include <unistd.h> // exit, sleep
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h> //O_RDWR	
+#include <fcntl.h> //O_RDWR
 #include <signal.h>
 #include <stdlib.h> // malloc, free, y otras
 #include <sys/ioctl.h>
 #include "../led_lkm.h"
 
-int main(int argc, char **argv) {
+#define DEFAULT_REPETITIONS 10
+
+int main(int argc, char **argv)
+{
     int fd, power;
 
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s device_file\n\
-  If device_file does not exist, create it using mknod(1) (as root)\n", argv[0]);
+    if (argc < 3)
+    {
+        fprintf(stderr, "Usage: %s device_file number_repetitions\n\
+  If device_file does not exist, create it using mknod(1) (as root)\n",
+                argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    if((fd = open(argv[1], O_RDWR, 0)) == -1) {
+    int number_repetitions = atoi(argv[3]);
+    if (number_repetitions == 0)
+    {
+        number_repetitions = DEFAULT_REPETITIONS;
+    }
+
+    if ((fd = open(argv[1], O_RDWR, 0)) == -1)
+    {
         perror("open");
         exit(EXIT_FAILURE);
     }
 
-    if (ioctl(fd, IOCTL_POWER_ON, 0) == -1) {
-        perror("ioctl IOCTL_POWER_ON failed"); 
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-    if (ioctl(fd, IOCTL_POWER_READ, &power) == -1) {
-        perror("ioctl IOCTL_POWER_READ failed"); 
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-    printf("Power: %d\n", power);
+    for (int i = 0; i < number_repetitions; i++)
+    {
+        if (ioctl(fd, IOCTL_POWER_ON, 0) == -1)
+        {
+            perror("ioctl IOCTL_POWER_ON failed");
+            close(fd);
+            exit(EXIT_FAILURE);
+        }
+        if (ioctl(fd, IOCTL_POWER_READ, &power) == -1)
+        {
+            perror("ioctl IOCTL_POWER_READ failed");
+            close(fd);
+            exit(EXIT_FAILURE);
+        }
+        printf("Power: %d (i=%d)\n", power, i);
+        sleep(1);
+        if (ioctl(fd, IOCTL_POWER_OFF, 0) == -1)
+        {
+            perror("ioctl IOCTL_POWER_OFF failed");
+            close(fd);
+            exit(EXIT_FAILURE);
+        }
 
-    sleep(10);
-
-    if (ioctl(fd, IOCTL_POWER_OFF, 0) == -1) {
-        perror("ioctl IOCTL_POWER_OFF failed"); 
-        close(fd);
-        exit(EXIT_FAILURE);
+        if (ioctl(fd, IOCTL_POWER_READ, &power) == -1)
+        {
+            perror("ioctl IOCTL_POWER_READ failed");
+            close(fd);
+            exit(EXIT_FAILURE);
+        }
+        sleep(1);
+        printf("Power: %d (i=%d)\n", power, i);
     }
 
-    if (ioctl(fd, IOCTL_POWER_READ, &power) == -1) {
-        perror("ioctl IOCTL_POWER_READ failed"); 
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-    printf("Power: %d\n", power);
     close(fd);
-	exit(EXIT_SUCCESS);
-
+    exit(EXIT_SUCCESS);
 }
