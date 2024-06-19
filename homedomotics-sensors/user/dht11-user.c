c5d1e24d41f7774f9f319029daac02bfd8485939
Camila Alvarez <cam.alvarez.i@gmail.com>
2024-06-19 17:45:41 -0400

    Implemented user space program to test driver

--- homedomotics-sensors/user/dht11-user.c
+++ homedomotics-sensors/user/dht11-user.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "../include/dht11-data.h"
+
+#define DHT11_CHAR_DEVICE "/dev/dht11_module"
 
 int main(int argc, char **argv)
 {
-
+    struct dht11_measurement measurement;
+    int fd = 0;
+    if ((fd = open(DHT11_CHAR_DEVICE, O_RDONLY)) < -1)
+        return 1;
+    if (read(fd, &measurement, sizeof(struct dht11_measurement)) != sizeof(struct dht11_measurement))
+        return 1;
+    if (measurement.successful)
+        printf("Humidity: %d.%d\%RH Temperature: %d.%dºC\n");
+    else
+        printf("Invalid reading\n");
     return 0;
 }
